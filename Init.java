b11a0c729468ca7ec6b660deb97753b29c9aaa06
gx0529 <1500291522@qq.com>
2018-10-24 19:11:05 +0800

    观察在main方法中的初始值

---
+++ Init.java
@@ -0,0 +1,13 @@
+
+class Test{
+    public int i;
+    public char a;
+}
+public class Init{
+    public static void main(String[] args){
+        Test test = new Test();
+        System.out.print(test.a);
+        System.out.print(test.i);
+    }
+}
+
