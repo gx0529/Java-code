25ecd72d1f41aaf00f66ac74c75c1cf501bf079a
gx0529 <1500291522@qq.com>
2018-11-14 23:49:38 +0800

    提交出错

---
+++ ReverseList.java
@@ -0,0 +1,37 @@
+package www.bit.java.util;
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode p1 = null;
+        ListNode p2 = head;
+        ListNode p3 = null;
+        if(head.next != null){
+            p3 = head.next;
+        }
+        while(p2 != null){
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+            if(p3 != null){
+                p3 = p3.next;
+            }
+        }
+        return p1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
