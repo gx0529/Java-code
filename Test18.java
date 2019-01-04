a94d7b5bce626a35b9c2c0d813154227a8857772
gx0529 <1500291522@qq.com>
2019-01-04 21:24:14 +0800

    整数反转

--- Test18.java
+++ Test18.java
@@ -1,4 +1,4 @@
-package www.bit.java;
+﻿package www.bit.java;
 
 /**
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
@@ -15,41 +15,58 @@ package www.bit.java;
  * 输入: 120
  * 输出: 21
  */
-class Solution {
+//class Solution {
+//    public int reverse(int x) {
+//        try {
+//            if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) {
+//                return 0;
+//            } else {
+//                String str = String.valueOf(x);
+//                StringBuilder stringBuilder = new StringBuilder();
+//                if (str.startsWith("-")) {
+//                    stringBuilder.append(str.substring(1, str.length()));
+//                    str = str.substring(0, 1) + stringBuilder.reverse().toString();
+//                    return Integer.parseInt(str);
+//                } else if (str.startsWith("0") && str.length() > 1) {
+//                    stringBuilder.append(str.substring(1, str.length()));
+//                    str = stringBuilder.reverse().toString();
+//                    return Integer.parseInt(str);
+//                } else {
+//                    stringBuilder.append(str);
+//                    str = stringBuilder.reverse().toString();
+//                    return Integer.parseInt(str);
+//                }
+//            }
+//        } catch (Exception e) {
+//            return 0;
+//        }
+//    }
+//}
+class Solution{
     public int reverse(int x) {
+        boolean flag = true;
+        if (x < 0) {
+            x = Math.abs(x);
+            flag = false;
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(x);
         try {
-            if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) {
-                return 0;
-            } else {
-                String str = String.valueOf(x);
-                StringBuilder stringBuilder = new StringBuilder();
-                if (str.startsWith("-")) {
-                    stringBuilder.append(str.substring(1, str.length()));
-                    str = str.substring(0, 1) + stringBuilder.reverse().toString();
-                    return Integer.parseInt(str);
-                } else if (str.startsWith("0") && str.length() > 1) {
-                    stringBuilder.append(str.substring(1, str.length()));
-                    str = stringBuilder.reverse().toString();
-                    return Integer.parseInt(str);
-                } else {
-                    stringBuilder.append(str);
-                    str = stringBuilder.reverse().toString();
-                    return Integer.parseInt(str);
-                }
+            int a = Integer.parseInt(stringBuffer.reverse().toString());
+            if (flag) {
+                return a;
+            }else {
+                return -a;
             }
         } catch (Exception e) {
             return 0;
         }
     }
 }
-
 public class Test2{
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int num = -123;
+        int num = 123;
         System.out.println(solution.reverse(num));
     }
 }
-
-
-
