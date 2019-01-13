df358e5ff34d5307cc6010ce1baafbd71e06d0e9
gx0529 <1500291522@qq.com>
2019-01-13 21:25:36 +0800

    完美数

---
+++ Test22.java
@@ -0,0 +1,44 @@
+package www.bit.java;
+
+/**
+ *对于一个 正整数，如果它和除了它自身以外的所有正因子之和相等，我们称它为“完美数”。
+ *给定一个 正整数 n， 如果他是完美数，返回 True，否则返回 False
+ *
+ * 示例：
+ * 输入: 28
+ * 输出: True
+ * 解释: 28 = 1 + 2 + 4 + 7 + 14
+ */
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num <= 0){
+            return false;
+        }
+        int count = 0;
+        for(int i=1; i<=num/2; i++){
+            if(num % i == 0){
+                count = count+i;
+            }
+        }
+        if(num == count){
+            return true;
+        }
+        return false;
+    }
+}
+public class Test2{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.checkPerfectNumber(28));
+    }
+}
+
+
+
+
+
+
+
+
+
