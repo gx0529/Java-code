e746e2d29725b6d223ab4d876b528314ad97cea9
gx0529 <1500291522@qq.com>
2019-02-12 19:56:30 +0800

    最大子序和

---
+++ Test58.java
@@ -0,0 +1,46 @@
+package www.bit.java;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int tmp = nums[0];
+        int sum = 0;
+        for(int num : nums){
+            if(sum > 0){
+                sum += num;
+            }
+            else{
+                sum = num;
+            }
+            tmp = Math.max(tmp, sum);
+        }
+        return tmp;
+    }
+}
+public class Test2{
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
