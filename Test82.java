e8cb3b60da3b33433657a9a8b27e57ee879aff2e
gx0529 <1500291522@qq.com>
2019-02-20 21:20:57 +0800

    N叉树的前序遍历

---
+++ Test82.java
@@ -0,0 +1,31 @@
+给定一个 N 叉树，返回其节点值的前序遍历。
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if(root == null){
+            return list;
+        }
+        list.add(root.val);
+        for(Node child : root.children){
+            preorder(child);
+        }
+        return list;
+    }
+}
