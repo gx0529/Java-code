e108d3c44feca238d24f1ae529c89f77ba5a73b7
gx0529 <1500291522@qq.com>
2019-05-25 19:21:14 +0800

    删除公共字符

---
+++ Test182.java
@@ -0,0 +1,19 @@
+
+É¾³ý¹«¹²×Ö·û
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNext()){
+            char[] ch = scanner.nextLine().toCharArray();
+            String str = scanner.nextLine();
+            for(int i=0; i<ch.length; i++){
+                if(!str.contains(String.valueOf(ch[i]))){
+                    System.out.print(ch[i]);
+                }
+            }
+        }
+    }
+}
